cbcce5d235b75ee6553b8eb869d794c8f4563ace
Porcupine <Porcupine@DESKTOP-OVN06OJ>
2024-10-17 16:06:07 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            checkBox1 = new CheckBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 28F);
+            label1.ImageAlign = ContentAlignment.TopCenter;
+            label1.Location = new Point(527, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(159, 51);
+            label1.TabIndex = 0;
+            label1.Text = "Đăng ký";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(454, 150);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(370, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(454, 212);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(370, 23);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft Sans Serif", 16F);
+            button1.Location = new Point(552, 350);
+            button1.Name = "button1";
+            button1.Size = new Size(155, 44);
+            button1.TabIndex = 2;
+            button1.Text = "Đăng ký";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.Location = new Point(339, 150);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Email";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F);
+            label3.Location = new Point(323, 212);
+            label3.Name = "label3";
+            label3.Size = new Size(91, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Password";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14F);
+            label4.Location = new Point(276, 270);
+            label4.Name = "label4";
+            label4.Size = new Size(164, 25);
+            label4.TabIndex = 5;
+            label4.Text = "Confirm Password";
+            label4.Click += label4_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Location = new Point(454, 272);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(370, 23);
+            textBox3.TabIndex = 6;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(454, 316);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(104, 19);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "Hiện mật khẩu";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14F);
+            label5.ForeColor = Color.Red;
+            label5.Location = new Point(875, 150);
+            label5.Name = "label5";
+            label5.Size = new Size(63, 25);
+            label5.TabIndex = 8;
+            label5.Text = "label5";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 14F);
+            label6.ForeColor = Color.Red;
+            label6.Location = new Point(875, 212);
+            label6.Name = "label6";
+            label6.Size = new Size(63, 25);
+            label6.TabIndex = 9;
+            label6.Text = "label6";
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 14F);
+            label7.ForeColor = Color.Red;
+            label7.Location = new Point(875, 272);
+            label7.Name = "label7";
+            label7.Size = new Size(63, 25);
+            label7.TabIndex = 10;
+            label7.Text = "label7";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1332, 547);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox3;
+        private CheckBox checkBox1;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System.Text.RegularExpressions;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static string emailPattern = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+        static string passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            textBox2.UseSystemPasswordChar = true;
+            textBox3.UseSystemPasswordChar = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+
+            string email = textBox1.Text;
+            if (string.IsNullOrWhiteSpace(emailPattern))
+            {
+                label5.Text = "Vui long nhap email";
+                label5.ForeColor = System.Drawing.Color.Red;
+                label5.Visible = true;
+
+            }
+
+            else if (!Regex.IsMatch(email, emailPattern))
+            {
+                label5.Text = "Email khong hop le";
+                label5.Visible = true;
+
+            }
+            else
+            {
+                label5.Visible = false;
+            }
+
+ 
+            if (string.IsNullOrWhiteSpace(passwordPattern))
+            {
+                label6.Text = "Vui long nhap mat khau";
+                label6.Visible = true;
+
+            }
+            else
+            {
+                label6.Visible = false;
+            }
+
+
+            if (string.IsNullOrWhiteSpace(passwordPattern))
+            {
+                label7.Text = "Vui long nhap lai mat khau";
+                label7.Visible = true;
+
+            }
+            else if (textBox2.Text != textBox3.Text)
+            {
+                label7.Text = "Mat khau khong khop";
+                label7.Visible = true;
+            }
+            else
+            {
+                label7.Text = " ";
+                label7.Visible = false;
+            }
+
+
+            string password = textBox2.Text;
+            string confirmPassword = textBox3.Text;
+
+
+            if (!Regex.IsMatch(password, passwordPattern))
+            {
+                MessageBox.Show("Mat khau khong hop le. Mat khau phai co it nhat 8 ky tu, bao gom chu thuong, chu hoa, so va ky tu dac biet.", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else if (password != confirmPassword)
+            {
+                MessageBox.Show("Mat khau va nhap lai mat khau khong khop.", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Dang ky thanh cong!", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox2.UseSystemPasswordChar = false;
+                textBox3.UseSystemPasswordChar = false;
+            }
+
+            else
+            {
+                textBox2.UseSystemPasswordChar = true;
+                textBox3.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
